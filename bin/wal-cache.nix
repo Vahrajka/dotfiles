8ad11ade09f5c4d81f5bd4aa0d837e5ef221158e
Zayd <zayd@example.com>
2025-07-04 22:16:45 +0200

    fixed error for multiple monitors

--- bin/wal-cache.nix
+++ bin/wal-cache.nix
@@ -2,11 +2,12 @@
 
 pkgs.writeShellScriptBin "wal-cache" ''
   # Define cache directory
-  CACHE_DIR="$HOME/dotfiles/bin/current-wallpaper"
+  CACHE_DIR="$HOME/dotfiles/bin"
 
   # Fetch the wallpaper path from swww
-  wallpaper_path=$(${pkgs.swww}/bin/swww query | awk '{print $8}')
+  wallpaper_path=$(${pkgs.swww}/bin/swww query | head -n1 | awk '{print $8}')
 
   # Copy the wallpaper to the cache directory (if it's not already there)
-  cp "$wallpaper_path" "$CACHE_DIR"
+  cp "$wallpaper_path" "$CACHE_DIR"/current-wallpaper
+
 ''
