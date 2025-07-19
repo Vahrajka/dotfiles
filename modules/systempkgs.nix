9b8cf0075b5e079db4682a5a5bb821a36b6db5e6
Zayd <zayd@example.com>
2025-07-19 16:02:48 +0200

    wlogout

--- modules/systempkgs.nix
+++ modules/systempkgs.nix
@@ -14,7 +14,6 @@
     (import ./../bin/brave-search.nix { inherit pkgs;})
     (callPackage ./../sddm-theme/sddm-theme.nix {}).sddm-theme
     (callPackage ./../sddm-theme/darkouls-sddm.nix {}).darksouls-sddm
-    (callPackage ./../sddm-theme/SilentSDDM.nix {}).SilentSDDM
     (callPackage ./../grub-themes/CyberGRUB-2077.nix {}).CyberGRUB-2077
     (callPackage ./../grub-themes/hollow-knight-grub.nix {}).hollow-knight-grub
     (callPackage ./../grub-themes/LainGrubTheme.nix {}).LainGrubTheme
--- sddm-theme/darkouls-sddm.nix
+++ sddm-theme/darkouls-sddm.nix
@@ -7,7 +7,7 @@
       owner = "Vahrajka";
       repo = "darksouls-sddm";
       rev = "36e3717d5832cc346c520010f1346c3d37ca71d8";
-      sha256 = "";
+      sha256 = "sha256-P+lR4UubYm9/kPfFDpZLsleG/yytvQO/qkmhMHA5gvM=";
       };
     dontBuild = true;
     installPhase = ''
